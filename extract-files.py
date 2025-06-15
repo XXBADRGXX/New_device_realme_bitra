ec18d4dc4225331fdc7c7c57659dbd37ede75df6
ryukftw <ryukv009@gmail.com>
2025-06-15 17:00:38 +0000

    RMX3371: Import device specific extract script

--- extract-files.py
+++ extract-files.py
@@ -26,7 +26,7 @@ from extract_utils.utils import (
 )
 
 namespace_imports = [
-    'device/oneplus/sm8250-common',
+    'device/realme/RMX3371',
     'hardware/oplus',
     'hardware/qcom-caf/sm8250',
     'hardware/qcom-caf/wlan',
@@ -137,8 +137,8 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'sm8250-common',
-    'oneplus',
+    'RMX3371',
+    'realme',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
